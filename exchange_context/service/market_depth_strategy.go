b8b698f2fee60ae2c27aa765dc38258611f5e007
Andrey Mashukov <amashukov@opensoft.team>
2023-09-12 21:16:31 +0800

    limit validation fixes

--- exchange_context/service/market_depth_strategy.go
+++ exchange_context/service/market_depth_strategy.go
@@ -20,7 +20,7 @@ func (m *MarketDepthStrategy) Decide(depth ExchangeModel.Depth) ExchangeModel.De
 			Score:        30.00,
 			Operation:    "SELL",
 			Timestamp:    time.Now().Unix(),
-			Price:        depth.GetBestBid(),
+			Price:        depth.GetBestAsk(),
 			Params:       [3]float64{buyVolume, sellVolume, 0},
 		}
 	}
@@ -33,7 +33,7 @@ func (m *MarketDepthStrategy) Decide(depth ExchangeModel.Depth) ExchangeModel.De
 			Score:        30.00,
 			Operation:    "BUY",
 			Timestamp:    time.Now().Unix(),
-			Price:        depth.GetBestAsk(),
+			Price:        depth.GetBestBid(),
 			Params:       [3]float64{buyVolume, sellVolume, 0},
 		}
 	}
@@ -43,7 +43,7 @@ func (m *MarketDepthStrategy) Decide(depth ExchangeModel.Depth) ExchangeModel.De
 		Score:        30.00,
 		Operation:    "HOLD",
 		Timestamp:    time.Now().Unix(),
-		Price:        0.00,
-		Params:       [3]float64{0, 0, 0},
+		Price:        depth.GetBestBid(),
+		Params:       [3]float64{buyVolume, sellVolume, 0},
 	}
 }

2f388e8a11d242e2f642b73991446859ff0af85c
Andrey Mashukov <amashukov@opensoft.team>
2023-09-14 22:44:53 +0800

    chart improvements

--- exchange_context/model/chart_point.go
+++ exchange_context/model/chart_point.go
@@ -4,3 +4,11 @@ type ChartPoint struct {
 	XAxis int64   `json:"x"` // date (timestamp)
 	YAxis float64 `json:"y"` // price
 }
+
+type FinancialPoint struct {
+	XAxis int64   `json:"x"`
+	Open  float64 `json:"o"`
+	High  float64 `json:"h"`
+	Low   float64 `json:"l"`
+	Close float64 `json:"c"`
+}

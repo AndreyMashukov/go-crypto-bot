12ab268216761c190bbaa3951947284eb5d6343a
Andrey Mashukov <amashukov@opensoft.team>
2023-09-15 22:46:42 +0800

    chart cache

--- exchange_context/controller/exchange_controller.go
+++ exchange_context/controller/exchange_controller.go
@@ -1,17 +1,22 @@
 package controller
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	ExchangeRepository "gitlab.com/open-soft/go-crypto-bot/exchange_context/repository"
 	"gitlab.com/open-soft/go-crypto-bot/exchange_context/service"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type ExchangeController struct {
 	ExchangeRepository *ExchangeRepository.ExchangeRepository
 	ChartService       *service.ChartService
+	RDB                *redis.Client
+	Ctx                *context.Context
 }
 
 func (e *ExchangeController) GetKlineListAction(w http.ResponseWriter, req *http.Request) {
@@ -48,10 +53,17 @@ func (e *ExchangeController) GetTradeListAction(w http.ResponseWriter, req *http
 }
 
 func (e *ExchangeController) GetChartListAction(w http.ResponseWriter, req *http.Request) {
-	chart := e.ChartService.GetCharts()
-	encoded, _ := json.Marshal(chart)
+	encoded := e.RDB.Get(*e.Ctx, "chart-cache").Val()
+
+	if len(encoded) == 0 {
+		chart := e.ChartService.GetCharts()
+		encodedRes, _ := json.Marshal(chart)
+		encoded = string(encodedRes)
+		e.RDB.Set(*e.Ctx, "chart-cache", encoded, time.Second*5)
+	}
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(encoded))
+	fmt.Fprintf(w, encoded)
 }

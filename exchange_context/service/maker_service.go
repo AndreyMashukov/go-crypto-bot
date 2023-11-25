38447b86672140070283e17cf51f96c5fae5ad4b
Andrei Mashukov <a.mashukoff@gmail.com>
2023-11-25 09:45:43 +0800

    fix ban

--- exchange_context/service/maker_service.go
+++ exchange_context/service/maker_service.go
@@ -559,7 +559,7 @@ func (m *MakerService) waitExecution(binanceOrder ExchangeModel.BinanceOrder, se
 				break
 			}
 
-			time.Sleep(time.Second * 10)
+			time.Sleep(time.Second * 15)
 			continue
 		}
 
@@ -616,7 +616,7 @@ func (m *MakerService) waitExecution(binanceOrder ExchangeModel.BinanceOrder, se
 			break
 		}
 
-		time.Sleep(time.Second * 10)
+		time.Sleep(time.Second * 15)
 	}
 
 	cancelOrder, err := m.Binance.CancelOrder(binanceOrder.Symbol, binanceOrder.OrderId)

73fa0a2f6f4acee9c85a7036aaa3e9d02b0a669d
Andrei Mashukov <a.mashukoff@gmail.com>
2023-11-25 22:42:08 +0800

    qty formatter fix

--- exchange_context/service/formatter.go
+++ exchange_context/service/formatter.go
@@ -30,15 +30,18 @@ func (m *Formatter) FormatQuantity(limit ExchangeModel.TradeLimit, quantity floa
 		return limit.MinQuantity
 	}
 
+	splitQty := strings.Split(fmt.Sprintf("%s", strconv.FormatFloat(quantity, 'f', -1, 64)), ".")
 	split := strings.Split(fmt.Sprintf("%s", strconv.FormatFloat(limit.MinQuantity, 'f', -1, 64)), ".")
 	precision := 0
 	if len(split) > 1 {
 		precision = len(split[1])
 	}
 
-	expression := "%." + strconv.Itoa(precision) + "f"
-	value := fmt.Sprintf(expression, quantity)
-	quantity, _ = strconv.ParseFloat(value, 64)
+	second := "00"
+	if precision > 0 && len(splitQty) > 1 {
+		second = splitQty[1][0:precision]
+	}
+	quantity, _ = strconv.ParseFloat(fmt.Sprintf("%s.%s", splitQty[0], second), 64)
 
 	return quantity
 }

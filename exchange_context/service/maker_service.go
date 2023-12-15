4123e9e6f0a84048e6dcc7fa41aa8d7a2dfae664
Andrei Mashukov <a.mashukoff@gmail.com>
2023-12-15 23:24:25 +0800

    Fixed

--- exchange_context/service/maker_service.go
+++ exchange_context/service/maker_service.go
@@ -754,7 +754,7 @@ func (m *MakerService) Sell(tradeLimit ExchangeModel.TradeLimit, opened Exchange
 
 	// commission can be around 0.4% (0.2% to one side)
 	// @see https://www.binance.com/en/fee/trading
-	if (totalExecuted + (totalExecuted * 0.004)) >= opened.ExecutedQuantity {
+	if (totalExecuted + (totalExecuted * 0.004 * float64(len(closings)))) >= opened.ExecutedQuantity {
 		opened.Status = "closed"
 	}
 

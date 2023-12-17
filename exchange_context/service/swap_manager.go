75b0981a3a50ade5c6d6dc83a385a334a61509c2
Andrei Mashukov <a.mashukoff@gmail.com>
2023-12-17 09:17:53 +0800

    swap update

--- exchange_context/service/swap_manager.go
+++ exchange_context/service/swap_manager.go
@@ -132,7 +132,7 @@ func (s *SwapManager) BuyBuySell(symbol string) BBSArbitrageChain {
 
 		option0Price := option0.SellPrice
 		// first sell is -2 points
-		option0Price -= option0.MinPrice * 2
+		// option0Price -= option0.MinPrice * 2
 		option0Price = s.Formatter.FormatPrice(option0, option0Price)
 		buy0Quantity := initialBalance //s.Formatter.FormatQuantity(option0, initialBalance)
 
@@ -161,7 +161,7 @@ func (s *SwapManager) BuyBuySell(symbol string) BBSArbitrageChain {
 
 			option1Price := option1.SellPrice
 			// sell -3 points
-			option1Price -= option1.MinPrice * 2
+			// option1Price -= option1.MinPrice * 2
 			option1Price = s.Formatter.FormatPrice(option1, option1Price)
 			buy1Quantity := buy0.Balance //s.Formatter.FormatQuantity(option1, buy0.Balance)
 
@@ -190,7 +190,7 @@ func (s *SwapManager) BuyBuySell(symbol string) BBSArbitrageChain {
 
 				option2Price := option2.BuyPrice
 				// buy +4 points
-				option2Price += option2.MinPrice * 4
+				// option2Price += option2.MinPrice * 4
 				option2Price = s.Formatter.FormatPrice(option2, option2Price)
 				sell1Quantity := buy1.Balance //s.Formatter.FormatQuantity(option2, buy1.Balance)
 

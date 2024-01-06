c5506b574b750b8d40f7d9b9e5b338933e24fb2f
Andrey Mashukov <amashukov@opensoft.team>
2024-01-06 17:51:02 +0800

    dataset for alt coins improved

--- exchange_context/service/python_ml_bridge.go
+++ exchange_context/service/python_ml_bridge.go
@@ -68,24 +68,11 @@ func (p *PythonMLBridge) setLearning(value bool) {
 	p.Learning = value
 }
 
-func (p *PythonMLBridge) LearnModel(symbol string) error {
-	p.setLearning(true)
-	defer p.setLearning(false)
-
-	datasetPath, err := p.DataSetBuilder.PrepareDataset(symbol)
-	if err != nil {
-		return err
-	}
-
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
-
-	defer os.Remove(datasetPath)
-
+func (p *PythonMLBridge) getPythonCode(symbol string, datasetPath string) string {
 	resultPath := p.getResultFilePath(symbol)
 	modelFilePath := p.getModelFilePath(symbol)
 
-	pyCode := fmt.Sprintf(string([]byte(`
+	return fmt.Sprintf(string([]byte(`
 price_dataset = pd.read_csv(
     filepath_or_buffer='%s',
     names=["open", "high", "low", "close", "volume", "sell_vol", "buy_vol"]
@@ -128,6 +115,79 @@ result_path = '%s'
 with open(result_path, 'w') as out:
     print(y_train_predict[0], file=out)
 `)), datasetPath, modelFilePath, resultPath)
+}
+
+func (p *PythonMLBridge) getPythonAltCoinCode(symbol string, datasetPath string) string {
+	resultPath := p.getResultFilePath(symbol)
+	modelFilePath := p.getModelFilePath(symbol)
+
+	return fmt.Sprintf(string([]byte(`
+price_dataset = pd.read_csv(
+    filepath_or_buffer='%s',
+    names=["open", "high", "low", "close", "volume", "sell_vol", "buy_vol", "btc_price"]
+)
+X = pd.DataFrame(np.c_[price_dataset['volume'], price_dataset['buy_vol'], price_dataset['sell_vol'], price_dataset['open'], price_dataset['low'],price_dataset['high'],price_dataset['btc_price']], columns = ['volume','buy_vol', 'sell_vol', 'open', 'low', 'high', 'btc_price'])
+Y = price_dataset['close']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=5)
+
+lin_model = LinearRegression()
+lin_model.fit(X_train, Y_train)
+y_train_predict = lin_model.predict(X_train)
+rmse = (np.sqrt(mean_squared_error(Y_train, y_train_predict)))
+r2 = sklearn.metrics.r2_score(Y_train, y_train_predict)
+
+print("The model performance for training set")
+print("--------------------------------------")
+print('RMSE is {}'.format(rmse))
+print('R2 score is {}'.format(r2))
+print("\n")
+
+# model evaluation for testing set
+y_test_predict = lin_model.predict(X_test)
+rmse = (np.sqrt(mean_squared_error(Y_test, y_test_predict)))
+r2 = sklearn.metrics.r2_score(Y_test, y_test_predict)
+
+print("The model performance for testing set")
+print("--------------------------------------")
+print('RMSE is {}'.format(rmse))
+print('R2 score is {}'.format(r2))
+model_file_path = '%s'
+with open(model_file_path, 'wb') as f:
+    pickle.dump(lin_model, f)
+
+lr2 = joblib.load(model_file_path)
+
+y_train_predict = lr2.predict(X_train.loc[[10]])
+print(y_train_predict)
+result_path = '%s'
+with open(result_path, 'w') as out:
+    print(y_train_predict[0], file=out)
+`)), datasetPath, modelFilePath, resultPath)
+}
+
+func (p *PythonMLBridge) LearnModel(symbol string) error {
+	p.setLearning(true)
+	defer p.setLearning(false)
+
+	datasetPath, err := p.DataSetBuilder.PrepareDataset(symbol)
+	if err != nil {
+		return err
+	}
+
+	p.Mutex.Lock()
+	defer p.Mutex.Unlock()
+
+	defer os.Remove(datasetPath)
+
+	resultPath := p.getResultFilePath(symbol)
+
+	var pyCode string
+	if "BTCUSDT" == symbol {
+		pyCode = p.getPythonCode(symbol, datasetPath)
+	} else {
+		pyCode = p.getPythonAltCoinCode(symbol, datasetPath)
+	}
 
 	pyCodeC := C.CString(pyCode)
 	defer C.free(unsafe.Pointer(pyCodeC))
@@ -142,6 +202,57 @@ with open(result_path, 'w') as out:
 	log.Printf("[%s] Result is: %s", symbol, text)
 
 	return nil
+}
+
+func (p *PythonMLBridge) GetPythonPredictCode(kLine ExchangeModel.KLine) string {
+	buyVolume, sellVolume := p.ExchangeRepository.GetTradeVolumes(kLine)
+	resultPath := p.getResultFilePath(kLine.Symbol)
+	modelFilePath := p.getModelFilePath(kLine.Symbol)
+
+	return fmt.Sprintf(string([]byte(`
+test = pd.DataFrame(np.c_[%f, %f, %f, %f, %f, %f], columns = ['volume','buy_vol', 'sell_vol', 'open', 'low', 'high'])
+
+lr2 = joblib.load('%s')
+a = lr2.predict(test)
+result_path = '%s'
+with open(result_path, 'w') as out:
+    print(a[0], file=out)
+`)),
+		kLine.Volume,
+		buyVolume,
+		sellVolume,
+		kLine.Open,
+		kLine.Low,
+		kLine.High,
+		modelFilePath,
+		resultPath,
+	)
+}
+
+func (p *PythonMLBridge) GetPythonPredictAltCoinCode(kLine ExchangeModel.KLine, btcPrice float64) string {
+	buyVolume, sellVolume := p.ExchangeRepository.GetTradeVolumes(kLine)
+	resultPath := p.getResultFilePath(kLine.Symbol)
+	modelFilePath := p.getModelFilePath(kLine.Symbol)
+
+	return fmt.Sprintf(string([]byte(`
+test = pd.DataFrame(np.c_[%f, %f, %f, %f, %f, %f, %f], columns = ['volume','buy_vol', 'sell_vol', 'open', 'low', 'high', 'btc_price'])
+
+lr2 = joblib.load('%s')
+a = lr2.predict(test)
+result_path = '%s'
+with open(result_path, 'w') as out:
+    print(a[0], file=out)
+`)),
+		kLine.Volume,
+		buyVolume,
+		sellVolume,
+		kLine.Open,
+		kLine.Low,
+		kLine.High,
+		btcPrice,
+		modelFilePath,
+		resultPath,
+	)
 }
 
 func (p *PythonMLBridge) Predict(symbol string) (float64, error) {
@@ -164,28 +275,19 @@ func (p *PythonMLBridge) Predict(symbol string) (float64, error) {
 		return 0.00, errors.New("price is unknown")
 	}
 
-	buyVolume, sellVolume := p.ExchangeRepository.GetTradeVolumes(*kLine)
-
 	resultPath := p.getResultFilePath(symbol)
 
-	pyCode := fmt.Sprintf(string([]byte(`
-test = pd.DataFrame(np.c_[%f, %f, %f, %f, %f, %f], columns = ['volume','buy_vol', 'sell_vol', 'open', 'low', 'high'])
+	var pyCode string
+	if "BTCUSDT" == symbol {
+		pyCode = p.GetPythonPredictCode(*kLine)
+	} else {
+		btcKline := p.ExchangeRepository.GetLastKLine("BTCUSDT")
+		if btcKline == nil {
+			return 0.00, errors.New("BTC price is unknown")
+		}
 
-lr2 = joblib.load('%s')
-a = lr2.predict(test)
-result_path = '%s'
-with open(result_path, 'w') as out:
-    print(a[0], file=out)
-`)),
-		kLine.Volume,
-		buyVolume,
-		sellVolume,
-		kLine.Open,
-		kLine.Low,
-		kLine.High,
-		modelFilePath,
-		resultPath,
-	)
+		pyCode = p.GetPythonPredictAltCoinCode(*kLine, btcKline.Close)
+	}
 
 	pyCodeC := C.CString(pyCode)
 	defer C.free(unsafe.Pointer(pyCodeC))

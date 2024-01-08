e1af2b9621af70297ab5357bf56f7ce3d7a82aab
Andrei Mashukov <a.mashukoff@gmail.com>
2024-01-08 21:08:20 +0800

    Balance check

--- exchange_context/service/maker_service.go
+++ exchange_context/service/maker_service.go
@@ -173,8 +173,6 @@ func (m *MakerService) Make(symbol string, decisions []ExchangeModel.Decision) {
 
 		if balanceErr != nil {
 			log.Printf("[%s] Min balance check: %s", tradeLimit.Symbol, balanceErr.Error())
-			time.Sleep(time.Minute)
-
 			return
 		}
 

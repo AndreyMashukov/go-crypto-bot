ab2cd5d88c2e0bb451005703edc4fa1891e31643
Andrey Mashukov <amashukov@opensoft.team>
2024-01-26 23:21:45 +0800

    Loss security class added

---
+++ tests/loss_security_test.go
@@ -0,0 +1,82 @@
+package tests
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/open-soft/go-crypto-bot/exchange_context/model"
+	"gitlab.com/open-soft/go-crypto-bot/exchange_context/service"
+	"testing"
+)
+
+func TestBuyPriceCorrection(t *testing.T) {
+	exchangeRepo := new(ExchangeTradeInfoMock)
+	binance := new(ExchangePriceAPIMock)
+
+	lossSecurity := service.LossSecurity{
+		MlEnabled:            true,
+		InterpolationEnabled: true,
+		Formatter:            &service.Formatter{},
+		ExchangeRepository:   exchangeRepo,
+		Binance:              binance,
+	}
+
+	assertion := assert.New(t)
+
+	limit := model.TradeLimit{
+		Symbol:                       "BTCUSDT",
+		BuyPriceHistoryCheckInterval: "1h",
+		BuyPriceHistoryCheckPeriod:   10,
+		MinProfitPercent:             2.40,
+		MinPrice:                     0.001,
+	}
+
+	binance.On("GetKLinesCached", limit.Symbol, limit.BuyPriceHistoryCheckInterval, limit.BuyPriceHistoryCheckPeriod).Return([]model.KLine{
+		{
+			High: 26000.00,
+		},
+		{
+			High: 25000.00,
+		},
+		{
+			High: 23000.00,
+		},
+		{
+			High: 23000.00,
+		},
+		{
+			High: 23000.00,
+		},
+		{
+			High: 24000.00,
+		},
+		{
+			High: 22000.00,
+		},
+		{
+			High: 23000.00,
+		},
+		{
+			High: 23000.00,
+		},
+		{
+			High: 23500.00,
+		},
+	})
+	kline := model.KLine{
+		Close:  23000.00,
+		Low:    22500.00,
+		Symbol: "BTCUSDT",
+	}
+	exchangeRepo.On("GetLastKLine", "BTCUSDT").Return(&kline)
+	exchangeRepo.On("GetPredict", "BTCUSDT").Return(22450.00, nil)
+	exchangeRepo.On("GetInterpolation", kline).Return(model.Interpolation{
+		Asset:                "BTC",
+		BtcInterpolationUsdt: 22430.00,
+		EthInterpolationUsdt: 22425.00,
+	}, nil)
+
+	price := lossSecurity.CheckBuyPriceOnHistory(limit, 25000.00)
+	assertion.Equal(22460.936999482725, price)
+
+	price = lossSecurity.BuyPriceCorrection(price, limit)
+	assertion.Equal(22425.00, price)
+}

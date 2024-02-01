8c1f4a3e30cb90368c6df25d22086f764abced0d
Andrei Mashukov <a.mashukoff@gmail.com>
2024-02-01 14:46:51 +0800

    API extended

--- exchange_context/model/trade_stack_item.go
+++ exchange_context/model/trade_stack_item.go
@@ -1,7 +1,13 @@
 package model
 
 type TradeStackItem struct {
-	Percent    Percent `json:"percent"`
-	Symbol     string  `json:"symbol"`
-	BudgetUsdt float64 `json:"budgetUsdt"`
+	Price            float64       `json:"price"`
+	IsPriceValid     bool          `json:"isPriceValid"`
+	Percent          Percent       `json:"percent"`
+	Symbol           string        `json:"symbol"`
+	BudgetUsdt       float64       `json:"budgetUsdt"`
+	HasEnoughBalance bool          `json:"hasEnoughBalance"`
+	BalanceAfter     float64       `json:"balanceAfter"`
+	BinanceOrder     *BinanceOrder `json:"binanceOrder"`
+	IsExtraCharge    bool          `json:"isExtraCharge"`
 }

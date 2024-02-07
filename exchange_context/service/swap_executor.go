ec102c1ea38ddeb523a1ac08b52ee5041182e2ce
Andrei Mashukov <a.mashukoff@gmail.com>
2024-02-07 14:49:54 +0800

    roll back min percent changed

--- exchange_context/service/swap_executor.go
+++ exchange_context/service/swap_executor.go
@@ -601,7 +601,7 @@ func (s *SwapExecutor) TryRollbackSwapTwo(
 		return errors.New("Swap chain type is not supported")
 	}
 
-	minSwapRollbackPercent := ExchangeModel.Percent(0.25)
+	minSwapRollbackPercent := ExchangeModel.Percent(0.75)
 
 	// pre-validate rollback...
 	swapPair, err := s.SwapRepository.GetSwapPairBySymbol(action.SwapOneSymbol)

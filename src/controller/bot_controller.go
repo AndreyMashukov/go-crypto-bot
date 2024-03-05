5c3160da0713d942f8f84d4460014cdf73796704
Andrew Mashukov <a.mashukoff@gmail.com>
2024-03-06 00:08:57 +0800

    ISSUE-6: Master Bot and Swap configuration API (#16)

--- src/controller/bot_controller.go
+++ src/controller/bot_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"gitlab.com/open-soft/go-crypto-bot/src/model"
+	"gitlab.com/open-soft/go-crypto-bot/src/repository"
 	"gitlab.com/open-soft/go-crypto-bot/src/service"
 	"net/http"
 )
@@ -11,9 +12,10 @@ import (
 type BotController struct {
 	HealthService *service.HealthService
 	CurrentBot    *model.Bot
+	BotRepository *repository.BotRepository
 }
 
-func (b *BotController) GetHealthCheck(w http.ResponseWriter, req *http.Request) {
+func (b *BotController) GetHealthCheckAction(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
@@ -29,4 +31,46 @@ func (b *BotController) GetHealthCheck(w http.ResponseWriter, req *http.Request)
 
 	encoded, _ := json.Marshal(health)
 	fmt.Fprintf(w, string(encoded))
+}
+
+func (b *BotController) PutConfigAction(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+
+	botUuid := req.URL.Query().Get("botUuid")
+
+	if botUuid != b.CurrentBot.BotUuid {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+
+		return
+	}
+
+	if req.Method != "PUT" {
+		http.Error(w, "Only PUT method is allowed", http.StatusMethodNotAllowed)
+
+		return
+	}
+
+	var botUpdate model.BotConfigUpdate
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(req.Body).Decode(&botUpdate)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	bot := b.BotRepository.GetCurrentBot()
+	bot.IsMasterBot = botUpdate.IsMasterBot
+	bot.IsSwapEnabled = botUpdate.IsSwapEnabled
+	err = b.BotRepository.Update(*bot)
+
+	if err != nil {
+		http.Error(w, "Couldn't update bot config.", http.StatusBadRequest)
+
+		return
+	}
 }
--- src/repository/bot_repository.go
+++ src/repository/bot_repository.go
@@ -3,16 +3,59 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"github.com/redis/go-redis/v9"
 	"gitlab.com/open-soft/go-crypto-bot/src/model"
 	"log"
 	"os"
+	"time"
 )
 
 type BotRepository struct {
 	DB  *sql.DB
 	RDB *redis.Client
 	Ctx *context.Context
+}
+
+func (b *BotRepository) GetCurrentBotCached(botId int64) model.Bot {
+	botUuid := os.Getenv("BOT_UUID")
+
+	if len(botUuid) == 0 {
+		panic("'BOT_UUID' variable must be set!")
+	}
+
+	cacheKey := b.GetCacheKey(botUuid)
+	cachedBot := b.RDB.Get(*b.Ctx, cacheKey).Val()
+
+	if len(cachedBot) > 0 {
+		var bot model.Bot
+		err := json.Unmarshal([]byte(cachedBot), &bot)
+		if err == nil {
+			if bot.Id != botId {
+				panic(fmt.Sprintf("Bot ID is different! %d != %d", bot.Id, botId))
+			}
+
+			return bot
+		}
+	}
+
+	bot := b.GetCurrentBot()
+
+	if bot == nil {
+		panic("Current bot is not found!")
+	}
+
+	if bot.Id != botId {
+		panic(fmt.Sprintf("Bot ID is different! %d != %d", bot.Id, botId))
+	}
+
+	botEncoded, err := json.Marshal(bot)
+	if err == nil {
+		b.RDB.Set(*b.Ctx, cacheKey, string(botEncoded), time.Minute)
+	}
+
+	return *bot
 }
 
 func (b *BotRepository) GetCurrentBot() *model.Bot {
@@ -27,12 +70,16 @@ func (b *BotRepository) GetCurrentBot() *model.Bot {
 	err := b.DB.QueryRow(`
 		SELECT 
 			b.id as Id, 
-			b.uuid as Uuid
+			b.uuid as Uuid,
+			b.is_master_bot as IsMasterBot,
+			b.is_swap_enabled as IsSwapEnabled
 		FROM bots b
 		WHERE b.uuid = ?`, botUuid,
 	).Scan(
 		&bot.Id,
 		&bot.BotUuid,
+		&bot.IsMasterBot,
+		&bot.IsSwapEnabled,
 	)
 
 	if err != nil {
@@ -52,4 +99,32 @@ func (b *BotRepository) Create(bot model.Bot) error {
 	}
 
 	return nil
+}
+
+func (b *BotRepository) Update(bot model.Bot) error {
+	_, err := b.DB.Exec(`
+		UPDATE bots b SET
+			b.is_swap_enabled = ?,
+			b.is_master_bot = ?
+	    WHERE b.uuid = ? AND b.id = ?
+	`,
+		bot.IsSwapEnabled,
+		bot.IsMasterBot,
+		bot.BotUuid,
+		bot.Id,
+	)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Invalidate cache
+	b.RDB.Del(*b.Ctx, b.GetCacheKey(bot.BotUuid))
+
+	return nil
+}
+
+func (b *BotRepository) GetCacheKey(botUuid string) string {
+	return fmt.Sprintf("bot-cached-%s", botUuid)
 }

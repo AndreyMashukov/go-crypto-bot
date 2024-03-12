151358aebd595c1aa31171248d5d04de07df71d8
Andrew Mashukov <a.mashukoff@gmail.com>
2024-03-12 23:35:45 +0800

    ISSUE-28: Price (Last Kline) updates with Mini Ticker (#30)

--- src/model/kline.go
+++ src/model/kline.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type KLine struct {
 	Symbol    string  `json:"s"`
@@ -10,25 +13,9 @@ type KLine struct {
 	High      float64 `json:"h,string"`
 	Interval  string  `json:"i"`
 	Timestamp int64   `json:"T,int"`
+	OpenTime  int64   `json:"t,int"`
 	Volume    float64 `json:"v,string"`
 	UpdatedAt int64   `json:"updatedAt"`
-	// 		"t": 1694420820000,
-	//      "T": 1694420879999,
-	//      "s": "BTCUSDT",
-	//      "i": "1m",
-	//      "f": 4075396873,
-	//      "L": 4075398403,
-	//      "o": "25760.40",
-	//      "c": "25756.30",
-	//      "h": "25760.40",
-	//      "l": "25753.60",
-	//      "v": "95.902",
-	//      "n": 1531,
-	//      "x": false,
-	//      "q": "2470213.54580",
-	//      "V": "51.693",
-	//      "Q": "1331459.78300",
-	//      "B": "0"
 }
 
 func (k *KLine) IsNegative() bool {
@@ -48,4 +35,15 @@ const PriceValidSeconds = 30
 func (k *KLine) IsPriceExpired() bool {
 	// todo: Find alternative way to get actual valid price
 	return (time.Now().Unix() - (k.UpdatedAt)) > PriceValidSeconds
+}
+
+func (k *KLine) Includes(ticker MiniTicker) bool {
+	return k.OpenTime <= ticker.EventTime && ticker.EventTime < k.Timestamp
+}
+
+func (k *KLine) Update(ticker MiniTicker) {
+	k.UpdatedAt = time.Now().Unix()
+	k.Close = ticker.Close
+	k.High = math.Max(k.High, ticker.Close)
+	k.Low = math.Min(k.Low, ticker.Close)
 }

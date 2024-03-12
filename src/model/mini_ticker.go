d11e5207cee5a260afba81cdcb617155360a5b03
Andrey Mashukov <amashukov@opensoft.team>
2024-03-12 23:28:57 +0800

    ISSUE-28: Price (Last Kline) updates with Mini Ticker

---
+++ src/model/mini_ticker.go
@@ -0,0 +1,16 @@
+package model
+
+type MiniTickerEvent struct {
+	MiniTicker MiniTicker `json:"data"`
+}
+
+type MiniTicker struct {
+	EventTime        int64   `json:"E"`
+	Symbol           string  `json:"s"`
+	Close            float64 `json:"c,string"`
+	Open             float64 `json:"o,string"`
+	High             float64 `json:"h,string"`
+	Low              float64 `json:"l,string"`
+	TotalVolumeAsset float64 `json:"v,string"`
+	TotalVolumeQuote float64 `json:"q,string"`
+}

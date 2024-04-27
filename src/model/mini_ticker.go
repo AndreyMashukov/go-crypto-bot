e4edd17c7bab22e9a495feaaf7a366c3c83afdd0
Andrei Mashukov <a.mashukoff@gmail.com>
2024-04-27 14:23:25 +0800

    Removed Open,High,Low from daily ticker

--- src/model/mini_ticker.go
+++ src/model/mini_ticker.go
@@ -8,9 +8,6 @@ type MiniTicker struct {
 	EventTime        TimestampMilli `json:"E"`
 	Symbol           string         `json:"s"`
 	Close            float64        `json:"c,string"`
-	Open             float64        `json:"o,string"`
-	High             float64        `json:"h,string"`
-	Low              float64        `json:"l,string"`
 	TotalVolumeAsset float64        `json:"v,string"`
 	TotalVolumeQuote float64        `json:"q,string"`
 }

8ac0124495e2113a33a2e67950be8a4d71653e2e
Andrey Mashukov <amashukov@opensoft.team>
2024-05-26 17:15:10 +0800

    Binance: Fixed exchange data update

--- src/service/strategy/market_trade_listener.go
+++ src/service/strategy/market_trade_listener.go
@@ -161,6 +161,7 @@ func (m *MarketTradeListener) ListenAll() {
 				go func(l model.TradeLimit) {
 					defer wg.Done()
 
+					// todo: If update never received, we lost coin price
 					k := m.ExchangeRepository.GetCurrentKline(l.Symbol)
 					if k != nil && k.IsPriceNotActual() {
 						lock.Lock()
